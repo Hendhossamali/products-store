109f66a8fde3c0bf4823c361b8dd272958662f98
hend <hendhossam36@gmail.com>
2023-12-04 21:33:13 +0200

    created-products-store

---
+++ src/app/products/products.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent {
+ products =[
+  {  id:1,
+    title:"glass",
+    price:100,
+    color:"black",
+    discount: 10,
+    photo:"../assets/products/product-1.avif",
+ },
+ {  id:2,
+  title:"table",
+  price:100,
+  color:"blue",
+  discount: 10,
+  photo:"../assets/products/product-2.avif",
+},
+{  id:3,
+  title:"shose",
+  price:100,
+  color:"red",
+  discount: 10,
+  photo:"../assets/products/product-3.avif",
+},
+{  id:4,
+  title:"cleancer",
+  price:100,
+  color:"yellow",
+  discount: 10,
+  photo:"../assets/products/product-4.avif",
+},
+{  id:5,
+  title:"car",
+  price:100,
+  color:"yellow",
+  discount: 10,
+  photo:"../assets/products/product-5.avif",
+},
+{  id:6,
+  title:"watch",
+  price:100,
+  color:"red",
+  discount: 10,
+  photo:"../assets/products/product-6.avif",
+},
+{  id:7,
+  title:"headphone",
+  price:100,
+  color:"black",
+  discount: 10,
+  photo:"../assets/products/product-7.avif"
+}
+]
+ }
